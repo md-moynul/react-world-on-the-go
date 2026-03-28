2999b497b177e0ae9cf20f53d81ee6e68a22870b
moynul447 <mmmdmoynulislam@gmail.com>
2026-03-28 14:02:07 +0600

    show all country in ui and add color when clicked visited button

---
+++ src/component/Country/Country.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './country.css'
+
+const Country = ({ country, handleVisitedCountries }) => {
+    const [visited, setVisited] = useState(false)
+    
+
+    // console.log(country.capital.capital[0]);
+    const handleVisit = () => {
+        // if(visited){
+        //     setVisited(false)
+        // }else{
+        //     setVisited(true)
+        // }
+        // setVisited(visited ? false : true)
+        setVisited(!visited)
+        handleVisitedCountries(country)
+    }
+
+    return (
+        <div className={`country  ${visited ? 'visited' : 'notVisited'}`}>
+            <img src={country.flags.flags.png} alt="" />
+            <h3>Name : {country.name.common}</h3>
+            <h4>Capital : {country.capital.capital[0] ? country.capital.capital[0] : 'unknown'}</h4>
+            <p>Population : {country.population.population}</p>
+            <button onClick={handleVisit}>{visited ? 'Visited' : 'Not Visited'}</button>
+        </div>
+    );
+};
+
+export default Country;
--- src/component/countries/Countries.jsx
+++ src/component/countries/Countries.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { use, useState } from 'react';
+import Country from '../Country/Country';
+import './countries.css'
 
-const Countries = () => {
+const Countries = ({ countriesPromise }) => {
+    const countriesData = use(countriesPromise);
+    const [visitedCountries , setVisitedCountries] = useState([])
+    const countries = countriesData.countries;
+    const handleVisitedCountries = (country) =>{
+        console.log('visited clicked',country);
+        const newVisitedCountries = [...visitedCountries,country];
+        setVisitedCountries(newVisitedCountries)
+        
+    }
+
     return (
         <div>
-            <h1>countries here</h1>
+            <h1>Countries are : {countries.length}</h1>
+            <h1>Visited Countries : {visitedCountries.length}</h1>
+            <div className='countries'>
+                {
+                    countries.map(
+                        country => <Country
+                            key={country.cca3.cca3}
+                            country={country}
+                            handleVisitedCountries={handleVisitedCountries}
+                        ></Country>)
+                }
+            </div>
+
         </div>
     );
-};
+    };
 
-export default Countries;
+    export default Countries;
